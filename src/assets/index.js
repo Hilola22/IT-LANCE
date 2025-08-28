94c3e14009d55ea1d6465a146451f197891c4df1
Hilola22 <hilolaorazaliyeva2203@gmail.com>
2025-08-28 06:42:59 +0500

    images changed

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,14 +4,12 @@ import card3 from "./card3.png";
 import card4 from "./card4.png";
 import card5 from "./card5.png";
 import card6 from "./card6.png";
-
 import icon1 from "./icon1.svg";
 import microphone from "./microphone.svg";
 import sensor from "./sensor.svg";
 import brightness from "./brightness.svg";
 import data from "./data.svg";
 import video from "./video.svg";
-
 export {
   card1,
   card2,
